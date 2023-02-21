f03a2c00b87cd6a8044f6afb8d5f1e9036daa246
Alexander Konukhov <jesusmisha@gmail.com>
2023-02-21 22:23:08 +0300

    completed first autotest

--- pages/index_page.py
+++ pages/index_page.py
@@ -4,9 +4,13 @@ from playwright.sync_api import Page
 
 class IndexPage:
     _BUTTON_GOOGLE_SEARCH = "//div[@class='FPdoLc lJ9FBc']//input[@name='btnK']"
+    _LINK_ENGLISH_LANG = "//a[contains(text(), 'English')]"
 
     def open_index_page(self, page: Page) -> None:
         page.goto(config.url.DOMAIN)
+
+    def press_link_english_lang(self, page: Page):
+        page.locator(self._LINK_ENGLISH_LANG).click()
 
     def get_text_from_google_search_button(self, page: Page):
         return page.locator(self._BUTTON_GOOGLE_SEARCH).get_attribute('value')
